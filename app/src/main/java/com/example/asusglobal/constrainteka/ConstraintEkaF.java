e25deda3047f690bca48e07b65dd4d432377a61b
ekafaradewi <44692821+ekafaradewi@users.noreply.github.com>
2019-02-14 19:11:25 +0700

    ConstraintEka

---
+++ app/src/main/java/com/example/asusglobal/constrainteka/ConstraintEkaF.java
@@ -0,0 +1,13 @@
+package com.example.asusglobal.constrainteka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintEkaF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_eka_f);
+    }
+}
